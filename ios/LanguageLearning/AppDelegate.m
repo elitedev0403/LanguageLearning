b5a6e88ed60465c39b3a3a331719bf78eab77167
elitedev0403 <iavosedra1108@gmail.com>
2022-03-08 09:07:55 -0800

    fix: ios

--- ios/LanguageLearning/AppDelegate.m
+++ ios/LanguageLearning/AppDelegate.m
@@ -4,10 +4,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#import <AppCenterReactNative.h>
-#import <AppCenterReactNativeAnalytics.h>
-#import <AppCenterReactNativeCrashes.h>
-
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -31,9 +27,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [AppCenterReactNative register];
-  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
-  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
